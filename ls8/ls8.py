93dd1ed4c030f497b28997bcdef298e29ba60a45
Yen Szen <55312065+yenszen@users.noreply.github.com>
2020-10-07 00:00:44 +0800

    completed day 2 mvp

--- ls8/ls8.py
+++ ls8/ls8.py
@@ -5,7 +5,17 @@
 import sys
 from cpu import *
 
-cpu = CPU()
+def main(argv):
+    if len(argv) != 2:
+        print(f"usage: {argv[0]} filename", file=sys.stderr)
+        return 1
 
-cpu.load()
-cpu.run()
+    cpu = CPU()
+
+    cpu.load(argv[1])
+    cpu.run()
+
+    return 0
+
+if __name__ == "__main__":
+    sys.exit(main(sys.argv))
